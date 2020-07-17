a3d9d84b52d5316eb3dbb0d1a986dd7f0c0856b4
Heba kaddouh <38936332+Hebakaddouh@users.noreply.github.com>
2020-07-17 13:07:22 +0200

    Update __init__.py

--- molvecgen/__init__.py
+++ molvecgen/__init__.py
@@ -1,4 +1,4 @@
-from vectorizers import SmilesVectorizer, ChemceptionVectorizer
-from generators import HetSmilesGenerator, SmilesGenerator, Iterator
-from generators import SmilesGenerator as ChemceptionGenerator
-from generators import SmilesSequence, Sequence
+from .vectorizers import SmilesVectorizer, ChemceptionVectorizer
+from .generators import HetSmilesGenerator, SmilesGenerator, Iterator
+from .generators import SmilesGenerator as ChemceptionGenerator
+from .generators import SmilesSequence, Sequence
